c2ca7ac5189b6dace933edac6d916afa93ae5495
PARMEZ <a.stefan@stefload.com>
2022-03-29 22:38:02 +0200

    its alive!

--- Guess the Number Game.py
+++ Guess the Number Game.py
@@ -1,15 +1,23 @@
 import random
 
 name = input('Podaj swoję imię: ')
-number = int(input('Podaj liczbę: '))
+number = int(input(f'Podaj liczbę od 1 do 25: '))
 
-bla = random.randint(1, 5)
+bla = random.randint(1, 25)
 
 if __name__ == "__main__":
-    if number == bla:
-        print(f'Brawo {name} zgadłeś!')
-    else:
-        print(f'Niestety {name}, nie udało Ci się!')
+    while number != bla:
+        if bla < number:
+            print(f'Przykro mi {name} Twoja liczba jest mniejsza od {number}!')
+            number = int(input("Podaj liczbę: "))
+        else:
+            print(f"Przykro mi {name} Twoja liczba jest większa od {number}!")
+            number = int(input("Podaj liczbę: "))
+
+print(f'Brawo {name} zgadłeś!')
+
+
+
 
 
 
