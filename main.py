2703872a7d3091eb8234bf9be5492852744b20fa
PARMEZ <a.stefan@stefload.com>
2022-03-29 21:49:58 +0200

    Second test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-game = input('Game: ')
+gamess = input('Game: ')
 food = input('Food: ')
 
-madlibs = f'I like playing {game} and i love eating {food}'
+madlibs = f'I like playing {gamess} and i love eating {food}'
 
 if __name__ == '__main__':
     print(madlibs)
