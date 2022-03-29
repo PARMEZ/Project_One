5137e94b9c4c65398bdab6321a82c877f42da81c
PARMEZ <89adamstefan@gmail.com>
2022-03-29 21:36:54 +0200

    New text

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-game = input('Game: ')
+games = input('Game: ')
 food = input('Food: ')
 
-madlibs = f'I like playing {game} and i love eating {food}'
+madlibs = f'I like playing {games} and i love eating {food}'
 
 if __name__ == '__main__':
     print(madlibs)
